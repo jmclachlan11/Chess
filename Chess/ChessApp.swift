dbd1e0568a12f294fa40b49bb4aeb66e5af46df3
Jacob McLachlan <wolvesfanusa@gmail.com>
2024-05-12 11:21:51 -0400

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Jacob McLachlan on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
